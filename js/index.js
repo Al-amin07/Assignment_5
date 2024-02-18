dd12cef61f906f5888af968f82c8db1ae7d1338b
Al-amin07 <alsujon2001@gmail.com>
2024-02-18 22:20:32 +0600

    Apply button is modified

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 //  const seats = document.querySelectorAll('.char-btn');
 const seats = document.getElementsByClassName('char-btn');
 let count = 0;
+let total = 0;
 for (let seat of seats) {
     seat.addEventListener('click', function (event) {
         const clickSeat = event.target;
         // clickSeat.classList.add('bg-green-600')
-
+        // total += 550;
+        // console.log(total)
         count++;
         if (count > 4) {
             alert('You Cannot booked more than 4 seats')
@@ -13,33 +15,37 @@ for (let seat of seats) {
         else {
             clickSeat.style.backgroundColor = '#1DD100'
             clickSeat.style.color = 'white';
+
+
             const seatremains = getIntValue('total-seat');
             const updateSeat = seatremains - 1;
             setElementById('total-seat', updateSeat);
+
+
             const booked = getIntValue('booked-seat');
             const updateBooked = booked + 1;
             setElementById('booked-seat', updateBooked);
-            const div = document.createElement('div');
+            
+
             const p1 = document.createElement('p');
             const p2 = document.createElement('p');
             const p3 = document.createElement('p');
             p1.innerText = clickSeat.innerText;
             p2.innerText = 'Economoy';
             p3.innerText = '550';
-            p2.style.textAlign = 'right'
-            p3.style.textAlign = 'right'
-            // div.classList.add('flex')
-            // div.classList.add('justyfy-between')
+            p2.style.textAlign = 'right';
+            p3.style.textAlign = 'right';
+            document.getElementById('append-container').appendChild(p1);
+            document.getElementById('append-container').appendChild(p2);
+            document.getElementById('append-container').appendChild(p3);
+
+            const total = getIntValue('total-tk');
+            const newTotal = total + 550;
+            setElementById('total-tk', newTotal);
+
            
-            // div.style.display = 'flex';
-            // div.style.justifyContent = 'space-between';
-            // div.appendChild(p1);
-            // div.appendChild(p2);
-            // div.appendChild(p3);
-           
-            document.getElementById('append-container').appendChild(p1)
-            document.getElementById('append-container').appendChild(p2)
-            document.getElementById('append-container').appendChild(p3)
+            setElementById('grand-total', newTotal);
+            clickSeat.parentNode.setAttribute('disabled',true)
             
         }
     })
@@ -57,3 +63,36 @@ function getIntValue(elementId) {
     const elementText = element.innerText;
     return parseFloat(elementText);
 }
+
+function getDiscount(elementId, dis){
+    const element = getIntValue(elementId);
+    return element * dis;
+}
+
+document.getElementById('apply').addEventListener('click', function(){
+    const input = document.getElementById('input-field');
+    const inputValue = input.value;
+    if(inputValue === 'NEW15'){
+        // const grandTotal = getDiscount('grand-total', 0.5);
+        // console.log(grandTotal)
+        const valueG = getIntValue('grand-total');
+        const newG = valueG * 0.85;
+        setElementById('grand-total', newG)
+        input.style.display = 'none';
+        document.getElementById('apply').style.display = 'none'
+
+        
+    }
+
+    else if(inputValue === 'Couple 20'){
+        // const grandTotal = getDiscount('grand-total', 0.5);
+        // console.log(grandTotal)
+        const valueG = getIntValue('grand-total');
+        const newG = valueG * 0.80;
+        setElementById('grand-total', newG)
+        input.style.display = 'none';
+        document.getElementById('apply').style.display = 'none'
+
+        
+    }
+})
